2396747dcf88095a854f92bceea2c4ebdd4dbc6f
Mikayel Ghazaryan <mk@mk.am>
2015-05-27 19:19:46 +0400

    timezone fix

--- src/AdminBundle/Form/ScheduleType.php
+++ src/AdminBundle/Form/ScheduleType.php
@@ -19,10 +19,14 @@ class ScheduleType extends AbstractType
             ->add('timeFrom', 'datetime', array(
                 'widget' => 'single_text',
                 'format' => 'yyyy-MM-dd hh:mm:ss',
+                'model_timezone' => $this->container->getParameter('model_timezone'),
+                'view_timezone' => $this->container->getParameter('view_timezone'),
                 'attr'   => array('class' => 'datepicker')))
             ->add('timeTo', 'datetime', array(
                 'widget' => 'single_text',
                 'format' => 'yyyy-MM-dd hh:mm:ss',
+                'model_timezone' => $this->container->getParameter('model_timezone'),
+                'view_timezone' => $this->container->getParameter('view_timezone'),
                 'attr'   => array('class' => 'datepicker')))
             ->add('bgImageUrl')
             ->add('room', 'choice', array(
